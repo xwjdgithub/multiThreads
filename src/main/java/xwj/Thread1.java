c071c7f3a3a73c4f3578a1c78fa263196ee3dff1
熊文静 <wenj.xiong@haihangyun.com>
2019-01-06 11:42:57 +0800

    Thread1修改：添加join测试

--- src/main/java/xwj/Thread1.java
+++ src/main/java/xwj/Thread1.java
@@ -8,6 +8,7 @@ public class Thread1 extends Thread {
     }
 
     public void run(){
+        System.out.println(Thread.currentThread().getName() + "开始运行");
         for(int i=0; i<5; i++){
             System.out.println(name + "运行" + i);
             try {
@@ -16,17 +17,7 @@ public class Thread1 extends Thread {
                 e.printStackTrace();
             }
         }
-    }
-
-    public static void main(String[] args){
-        Thread1 threadA = new Thread1("A");
-        Thread1 thread1B = new Thread1("B");
-        //注意不要直接调用ran方法，否则只能按顺序执行threadA和thread1B的run方法
-        //即thread1A的run方法执行完毕后，才开始执行thread1B的run方法
-//        threadA.run();
-//        thread1B.run();
-        threadA.start();
-        thread1B.start();
+        System.out.println(Thread.currentThread().getName() + "运行结束");
     }
 
 }
